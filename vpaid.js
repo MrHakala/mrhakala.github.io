3e38ee0536bcf3c625c25bbce328e30a1dd1c22e
MrHakala <hakala.miika@gmail.com>
2023-10-19 21:08:04 +0300

    Update vpaid.js

--- vpaid.js
+++ vpaid.js
@@ -9,6 +9,7 @@ VPAIDCreative.prototype.initAd = function (width, height, viewMode, desiredBitra
     // videoSlot is the object where any video element can be rendered
     this.videoSlot = environmentVars.videoSlot;
 
+
     // Let's create a simple anchor element as our "ad"
     this.anchor = document.createElement('a');
     this.anchor.href = 'https://example.com';
@@ -23,6 +24,35 @@ VPAIDCreative.prototype.initAd = function (width, height, viewMode, desiredBitra
     this.anchor.style.textAlign = 'center';
     this.slot.appendChild(this.anchor);
 }
+// Method to skip the ad
+VPAIDAd.prototype.skipAd = function() {
+// implementation of skipping ad
+console.log('Ad skipped.');
+};
+
+// Method to resize the ad
+VPAIDAd.prototype.resizeAd = function(width, height, viewMode) {
+// implementation of resizing ad
+console.log(`Ad resized to ${width}x${height}, view mode: ${viewMode}.`);
+};
+
+// Method to stop the ad
+VPAIDAd.prototype.stopAd = function() {
+// implementation of stopping ad
+console.log('Ad stopped.');
+};
+
+// Method to pause the ad
+VPAIDAd.prototype.pauseAd = function() {
+// implementation of pausing ad
+console.log('Ad paused.');
+};
+
+// Method to resume the ad
+VPAIDAd.prototype.resumeAd = function() {
+// implementation of resuming ad
+console.log('Ad resumed.');
+};
 
 function getVPAIDAd() {
     return new VPAIDCreative();
