b3590a6bd7b358ca2a4b9bede7ea964a68b58cfb
MrHakala <hakala.miika@gmail.com>
2023-10-20 19:04:25 +0300

    Update vpaid.js

--- vpaid.js
+++ vpaid.js
@@ -1,21 +1,10 @@
 var VpaidAd = function() {
-  // The slot is the div element on the main page that the ad is supposed to
-  // occupy.
   this.slot_ = null;
-  // The video slot is the video object that the creative can use to render the
-  // video element it might have.
   this.videoSlot_ = null;
-  // An object containing all registered events.  These events are all
-  // callbacks from the vpaid ad.
   this.eventCallbacks_ = {};
-  // A list of attributes getable and setable.
   this.attributes_ = {};
 };
 
-
-/**
- * Html to populate into the ad.  This provides all UI elements for the ad.
- */
 VpaidAd.HTML_TEMPLATE =
     '<div style="background:#108ccd; width:100%; height:100%">' +
     '<div style="height: 100%;' +
@@ -25,18 +14,6 @@ VpaidAd.HTML_TEMPLATE =
     '</div>';
 
 
-/**
- * VPAID defined init ad, initializes all attributes in the ad.  Ad will
- * not start until startAd is called.
- *
- * @param {number} width The ad width.
- * @param {number} height The ad heigth.
- * @param {string} viewMode The ad view mode.
- * @param {number} desiredBitrate The desired bitrate.
- * @param {Object} creativeData Data associated with the creative.
- * @param {Object} environmentVars Variables associated with the creative like
- *     the slot and video slot.
- */
 VpaidAd.prototype.initAd = function(
     width,
     height,
@@ -47,13 +24,8 @@ VpaidAd.prototype.initAd = function(
   // slot and videoSlot are passed as part of the environmentVars
   this.slot_ = environmentVars.slot;
   this.videoSlot_ = environmentVars.videoSlot;
-
-  this.log('initAd ' + width + 'x' + height +
-      ' ' + viewMode + ' ' + desiredBitrate);
   this.renderSlot_();
-  this.addButtonListeners_();
-  this.fillProperties_();
-  this.eventCallbacks_['AdLoaded']();
+  this.emit('AdLoaded');
 };
 
 
@@ -139,25 +111,12 @@ VpaidAd.prototype.resumeAd = function() {
   }
 };
 
-
-/**
- * Registers a callback for an event.
- * @param {Function} aCallback The callback function.
- * @param {string} eventName The callback type.
- * @param {Object} aContext The context for the callback.
- */
 VpaidAd.prototype.subscribe = function(aCallback, eventName, aContext) {
   this.log('Subscribe ' + aCallback);
   var callBack = aCallback.bind(aContext);
   this.eventCallbacks_[eventName] = callBack;
 };
 
-
-/**
- * Removes a callback based on the eventName.
- *
- * @param {string} eventName The callback type.
- */
 VpaidAd.prototype.unsubscribe = function(eventName) {
   this.log('unsubscribe ' + eventName);
   this.eventCallbacks_[eventName] = null;
@@ -181,85 +140,14 @@ VpaidAd.prototype.getAdIcons = function() {};
 VpaidAd.prototype.getAdLinear = function() {};
 VpaidAd.prototype.log = function(message) {};
 VpaidAd.prototype.eventSelected_ = function() {};
-
-
-/**
- * Callback for AdClickThru button.
- *
- * @private
- */
 VpaidAd.prototype.adClickThruHandler_ = function() {};
+VpaidAd.prototype.adErrorHandler_ = function() {};
+VpaidAd.prototype.adLogHandler_ = function() {};
+VpaidAd.prototype.adInteractionHandler_ = function () {};
 
-
-/**
- * Callback for AdError button.
- *
- * @private
- */
-VpaidAd.prototype.adErrorHandler_ = function() {
-  if (!this.isEventSubscribed_('AdError')) {
-    this.log('AdError function callback not subscribed.');
-    return;
-  }
-  var adError = document.getElementById('adErrorMsg').value;
-  this.log('adError(' + adError + ')');
-  this.eventCallbacks_['AdError'](adError);
-};
-
-
-/**
- * Callback for AdLogMsg button.
- *
- * @private
- */
-VpaidAd.prototype.adLogHandler_ = function() {
-  if (!this.isEventSubscribed_('AdLog')) {
-    this.log('Error: AdLog function callback not subscribed.');
-    return;
-  }
-  var adLogMsg = document.getElementById('adLogMsg').value;
-  this.log('adLog(' + adLogMsg + ')');
-  this.eventCallbacks_['AdLog'](adLogMsg);
-};
-
-
-/**
- * Callback for AdInteraction button.
- *
- * @private
- */
-VpaidAd.prototype.adInteractionHandler_ = function() {
-  if (!this.isEventSubscribed_('AdInteraction')) {
-    this.log('Error: AdInteraction function callback not subscribed.');
-    return;
-  }
-  var adInteraction = document.getElementById('adInteractionId').value;
-  this.log('adLog(' + adInteraction + ')');
-  this.eventCallbacks_['AdInteraction'](adInteraction);
-};
-
-/**
- * @param {string} eventName
- * @return {Boolean} True if this.eventCallbacks_ contains the callback.
- * @private
- */
-VpaidAd.prototype.isEventSubscribed_ = function(eventName) {
+VpaidAd.prototype.isEventSubscribed_ = function (eventName) {
   return typeof(this.eventCallbacks_[eventName]) === 'function';
 };
-
-
-/**
- * Populates all of the vpaid ad properties.
- *
- * @private
- */
-VpaidAd.prototype.fillProperties_ = function() {
-  for (var key in this.attributes_) {
-    var span = document.getElementById(key);
-    span.textContent = this.attributes_[key];
-  }
-};
-
 
 var getVPAIDAd = function() {
   return new VpaidAd();
